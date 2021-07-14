980f9bf24ea2dafce4c0226ffa1d4704ecc2b7d7
Carlos Alberto Gaviria Gomez <gaviria.carlos@correounivalle.edu.co>
2021-07-14 15:13:16 -0500

    v1.2.3
    Implementacion de estado y guarda los datos de imagenes en un objeto.

--- index.js
+++ index.js
@@ -3,9 +3,13 @@
 const videoElement = document.querySelector('#video');
 const captureElement = document.querySelector('#capture');
 const canvasScreen = document.querySelector('#screenshot');
-var foto1 = document.querySelector('#foto1');
-var foto1 = document.querySelector('#foto2');
+const instrucciones1 = document.querySelector('#instrucciones1');
+const instrucciones2 = document.querySelector('#instrucciones2');
+const boton1 = document.querySelector('#btn1');
+const boton2 = document.querySelector('#btn2');
 
+var state = {
+}
 
 const constraints = {
   video: {
@@ -26,25 +30,52 @@ const handleStream = (stream) => {
 };
 
 const startStream = async (constraints) => {
-  const stream = await navigator.mediaDevices.getUserMedia(constraints);
-  //const streamFoto = await navigator.mediaDevices.getUserMedia(constraints2);
-  handleStream(stream);
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleStream(stream);
+  }
+  catch (err) {
+    console.error(err);
+  }
+  
 };
 
 const doScreenshot = () => {
+
   canvasScreen.width = 500;
   canvasScreen.height = 889;
   let sH = videoElement.videoHeight;
   let sW = 0.5625*videoElement.videoHeight;
   let sX = (videoElement.videoWidth - sW) * 0.5;
   canvasScreen.getContext('2d').drawImage(videoElement, sX, 0, sW, sH, 0, 0, 500, 889);
-  var data = canvasScreen.toDataURL('image/jpg');
-  console.log('Foto tomada: ', data);
+  if (state.numPaso == 1) {
+    state.dataImage1 = canvasScreen.toDataURL('image/jpg');
+    instrucciones2.style.display = 'flex';
+    //console.log('Foto tomada: ', data);
+  }
+  else if (state.numPaso == 2) {
+    state.dataImage2 = canvasScreen.toDataURL('image/jpg');
+  }
 }
 
+/////////////////////////////////////////////////////////////////////////////
+
 if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
-  console.log("Let's get this party started");
+
+  console.log("El dispositivo es compatible!");
   startStream(constraints);
+
 }
 
 captureElement.addEventListener('click', doScreenshot, false);
+
+
+boton1.addEventListener('click', ()=>{
+  instrucciones1.style.display = 'none';
+  state.numPaso = 1;
+}, false);
+
+boton2.addEventListener('click', ()=>{
+  instrucciones2.style.display = 'none';
+  state.numPaso = 2;
+}, false);
